57d68a9f1d874dfbd894921ffcfc19c6c1f74bd3
Vinícius Oyama <vinicius.oyama@gmail.com>
2012-10-24 13:10:55 -0200

    rename the rest_api

---
+++ spec/rest_api/rest_api_spec.rb
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+describe "RestApi" do
+
+  it "should yield self" do 
+    RestApi.setup do |config|
+      config.should be == RestApi
+    end
+  end
+
+  it "should configure api_url - with slash" do 
+    RestApi.setup do |config|
+      config.api_url = "http://www.localhost.com:4000/"
+    end
+    RestApi.instance_eval { api_url }.should be == "http://www.localhost.com:4000/"
+  end
+
+
+  it "should configure api_url - whithout slash" do 
+    RestApi.setup do |config|
+      config.api_url = "http://www.localhost.com:4000"
+    end
+    RestApi.instance_eval { api_url }.should be == "http://www.localhost.com:4000/"
+  end
+
+  it "should return the request parser" do   
+    RestApi.request_parser.should be == RestApi::API::RequestParser
+  end
+
+end
